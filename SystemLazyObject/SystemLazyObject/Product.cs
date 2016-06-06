a0b78799eef6253b73e5226886409af5f3089682
Hoàng Phi <lokialicehd@gmail.com>
2016-06-06 13:49:23 +0700

    Lazy Object Initialization with System.Lazy of T class
    Ref:
    http://www.codeproject.com/Articles/612801/Lazy-Object-Initialization-with-System-Lazy-of-T-c

---
+++ SystemLazyObject/SystemLazyObject/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SystemLazyObject
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+
+        public Product()
+        {
+            Thread.Sleep(5000);
+        }
+
+        public static IList<Lazy<Product>> GetProductLazyListLazy()
+        {
+            var list = new List<Lazy<Product>>();
+            var product1 = new Lazy<Product>(() =>
+            new Product { Id = 1, Title = "Product 1" });
+            list.Add(product1);
+            var product2 = new Lazy<Product>(() =>
+            new Product { Id = 2, Title = "Product 2" });
+            list.Add(product2);
+            var product3 = new Lazy<Product>(() =>
+            new Product { Id = 3, Title = "Product 3" });
+            list.Add(product3);
+            return list;
+        }
+
+        public static IList<Product> GetProductList()
+        {
+            var product1 = new Product
+            {
+                Id = 1,
+                Title = "Product 1"
+            };
+            var product2 = new Product
+            {
+                Id = 2,
+                Title = "Product 2"
+            };
+            var product3 = new Product
+            {
+                Id = 3,
+                Title = "Product 3"
+            };
+
+            return new List<Product>(new[] { product1, product2, product3 });
+
+        }
+    }
+}
---
+++ SystemLazyObject/SystemLazyObject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemLazyObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            IList<Product> list = Product.GetProductList();
+            Console.WriteLine(watch.Elapsed.Seconds);
+            Console.WriteLine("Total products in my list: " + list.Count());
+
+            watch = Stopwatch.StartNew();
+            var listTemps = Product.GetProductLazyListLazy();
+            Console.WriteLine(watch.Elapsed.Seconds);
+            Console.WriteLine("Total products in my GetProductLazyListLazy: " + listTemps.Count());
+            watch = Stopwatch.StartNew();
+            foreach (var item in listTemps)
+            {
+                var itemTitle = item.Value.Title;
+            }
+            Console.WriteLine("Foreach {0}", watch.Elapsed.Seconds);
+            Console.ReadLine();
+        }
+    }
+}
